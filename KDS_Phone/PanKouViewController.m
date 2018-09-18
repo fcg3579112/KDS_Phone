42233947ea176b841ab7479b60bd74dfb2fe5d9f
冯传高 <chuangao.feng@inin88.com>
2018-09-18 09:05:56 +0800

    Feat: 添加盘口视图

---
+++ KDS_Phone/PanKouViewController.m
@@ -0,0 +1,38 @@
+//
+//  PanKouViewController.m
+//  KDS_Phone
+//
+//  Created by chuangao.feng on 2018/9/18.
+//  Copyright © 2018年 com.csc. All rights reserved.
+//
+
+#import "PanKouViewController.h"
+
+@interface PanKouViewController ()
+
+@end
+
+@implementation PanKouViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationController.navigationBarHidden = YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
